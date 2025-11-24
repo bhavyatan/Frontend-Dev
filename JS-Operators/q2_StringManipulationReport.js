6a597e424bc0d3fd37c6fdf4fc60b29606ab390b
bhavyatan <bhavya.t145@gmail.com>
2025-11-24 22:00:59 +0530

    [Bhavya] JavaScript Practice

--- JS-Operators/q2_StringManipulationReport.js
+++
@@ -1,5 +0,0 @@
-var product = " wireless headphones PRO ".trim().toLowerCase();
-product = product.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-product = product.replace('Pro', 'Pro Edition');
-console.log('Cleaned Title:', product);
-console.log('Length:', product.length);
--- JS-Operators/q7_CustomerFeedbackProcessor.js
+++
@@ -1,8 +0,0 @@
-var feedback = "Great product! Fast delivery and amazing sound quality!";
-var wordCount = feedback.split(' ').length;
-if (feedback.toLowerCase().includes('bad') || feedback.toLowerCase().includes('poor')) {
-  console.log('Needs Improvement');
-} else {
-  console.log('Positive Feedback');
-}
-console.log('Word Count:', wordCount);
